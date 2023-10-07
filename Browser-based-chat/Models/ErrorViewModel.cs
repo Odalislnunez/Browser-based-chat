7e38391e7cf1e65bce87e02c87a9d5bc9af71025
Odalis <odalislnunez@hotmail.com>
2023-10-07 18:05:09 -0400

    Add project files.

---
+++ Browser-based-chat/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Browser_based_chat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
